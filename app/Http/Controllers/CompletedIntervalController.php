cd6e32d39a10724257b6bc9443050b2fd46e89e0
Mahdi <rahnamamahdi78@gmail.com>
2025-12-28 16:31:24 +0330

    create necessary files

---
+++ app/Http/Controllers/CompletedIntervalController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\CompletedInterval;
+use App\Http\Requests\StoreCompletedIntervalRequest;
+use App\Http\Requests\UpdateCompletedIntervalRequest;
+
+class CompletedIntervalController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreCompletedIntervalRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(CompletedInterval $completedInterval)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(CompletedInterval $completedInterval)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateCompletedIntervalRequest $request, CompletedInterval $completedInterval)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(CompletedInterval $completedInterval)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/IntervalController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Interval;
+use App\Http\Requests\StoreIntervalRequest;
+use App\Http\Requests\UpdateIntervalRequest;
+
+class IntervalController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreIntervalRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Interval $interval)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Interval $interval)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateIntervalRequest $request, Interval $interval)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Interval $interval)
+    {
+        //
+    }
+}

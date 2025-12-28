3f0c5e147847c49ff295ad85d292dfc6118f2820
Mahdi <rahnamamahdi78@gmail.com>
2025-12-28 18:24:14 +0330

    create login and register panels and also build those process

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,9 +2,39 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\UserLoginRequest;
+use App\Http\Requests\UserRegisterRequest;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
-    //
+    public function loginForm(){
+        return view('auth.login');
+    }
+    public function login(UserLoginRequest $request){
+        $credentials = $request->only('email', 'password');
+        if (!Auth::attempt($credentials)) {
+            return redirect()->back()->with('error', 'Invalid Credentials');
+        }
+        return redirect()->route('dashboard');
+    }
+
+    public function registerForm()
+    {
+        return view('auth.register');
+    }
+    public function register(UserRegisterRequest $request){
+        $user=User::create($request->all());
+        if($user){
+            Auth::login($user);
+            return redirect()->route('dashboard');
+        }
+        return redirect()->back()->with('error', 'Something went wrong');
+    }
+    public function logout(){
+        Auth::logout();
+        return redirect()->route('login');
+    }
 }
--- app/Http/Requests/UserRegisterRequest.php
+++ app/Http/Requests/UserRegisterRequest.php
@@ -11,7 +11,7 @@ class UserRegisterRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +22,9 @@ class UserRegisterRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'name' => 'required',
+            'email' => 'required|email|unique:users',
+            'password' => 'required',
         ];
     }
 }
